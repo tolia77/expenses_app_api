ef0eb6dbf62b15c735a0ed34f6f635df3674f9d5
tolia k <tolia.kohan@gmail.com>
2026-04-15 22:21:03 +0300

    feat(04-02): update ReceiptsController to inject @CurrentUser() on all handlers
    - Import CurrentUser decorator from auth/decorators
    - Replace Patch with Put for update endpoint
    - All 5 handlers inject @CurrentUser() user and pass user.sub to service

--- src/receipts/receipts.controller.ts
+++ src/receipts/receipts.controller.ts
@@ -3,40 +3,41 @@ import {
   Get,
   Post,
   Body,
-  Patch,
+  Put,
   Param,
   Delete,
 } from '@nestjs/common';
 import { ReceiptsService } from './receipts.service';
 import { CreateReceiptDto } from './dto/create-receipt.dto';
 import { UpdateReceiptDto } from './dto/update-receipt.dto';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
 
 @Controller('receipts')
 export class ReceiptsController {
   constructor(private readonly receiptsService: ReceiptsService) {}
 
   @Post()
-  create(@Body() createReceiptDto: CreateReceiptDto) {
-    return this.receiptsService.create(createReceiptDto);
+  create(@CurrentUser() user, @Body() createReceiptDto: CreateReceiptDto) {
+    return this.receiptsService.create(user.sub, createReceiptDto);
   }
 
   @Get()
-  findAll() {
-    return this.receiptsService.findAll();
+  findAll(@CurrentUser() user) {
+    return this.receiptsService.findAll(user.sub);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.receiptsService.findOne(id);
+  findOne(@Param('id') id: string, @CurrentUser() user) {
+    return this.receiptsService.findOne(id, user.sub);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReceiptDto: UpdateReceiptDto) {
-    return this.receiptsService.update(id, updateReceiptDto);
+  @Put(':id')
+  update(@Param('id') id: string, @CurrentUser() user, @Body() updateReceiptDto: UpdateReceiptDto) {
+    return this.receiptsService.update(id, user.sub, updateReceiptDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.receiptsService.remove(id);
+  remove(@Param('id') id: string, @CurrentUser() user) {
+    return this.receiptsService.remove(id, user.sub);
   }
 }
