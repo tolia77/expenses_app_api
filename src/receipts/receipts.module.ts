ebb9b28baf3fd4bee0da47ce70f67f9def0eed9f
tolia k <tolia.kohan@gmail.com>
2026-04-18 16:45:44 +0300

    feat(07-03): wire StorageModule into ReceiptsModule and inject StorageService
    - add StorageModule to receipts.module.ts imports array
    - add StorageService constructor param to ReceiptsService
    - add BadRequestException and randomUUID imports in preparation for upload methods

--- src/receipts/receipts.module.ts
+++ src/receipts/receipts.module.ts
@@ -4,9 +4,10 @@ import { Receipt } from './entities/receipt.entity';
 import { ReceiptsService } from './receipts.service';
 import { ReceiptsController } from './receipts.controller';
 import { MerchantsModule } from '../merchants/merchants.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Receipt]), MerchantsModule],
+  imports: [TypeOrmModule.forFeature([Receipt]), MerchantsModule, StorageModule],
   controllers: [ReceiptsController],
   providers: [ReceiptsService],
   exports: [ReceiptsService],
