400b26e3c2118074586ccfe5ac109f179f9ee413
tolia k <tolia.kohan@gmail.com>
2026-04-19 11:05:51 +0300

    feat(08-04): add PaginationDto and Paginated response wrapper
    - add PaginationDto with page (min 1, default 1) + limit (1-100, default 20)
    - @Type(() => Number) coerces query-string ints via global transform
    - add Paginated(itemType) factory returning class with @Expose/@Type on data/meta
    - class instance (not plain object) ensures ClassSerializerInterceptor recurses into data[]
    - extend FilterReceiptsDto and SearchExpensesDto from PaginationDto

--- src/receipts/dto/filter-receipts.dto.ts
+++ src/receipts/dto/filter-receipts.dto.ts
@@ -1,6 +1,7 @@
 import { IsOptional, IsDateString } from 'class-validator';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
-export class FilterReceiptsDto {
+export class FilterReceiptsDto extends PaginationDto {
   @IsOptional()
   @IsDateString()
   from?: string;
